29fc5e733b8511c9f2cb0808ba0d0bdcc88af2bb
Sriharikrishna Agnihotram <srihari667@gmail.com>
2023-03-06 06:53:47 +0000

    Greet Card Solution

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  // Write your code here.
+  <div className="container">
+    <h1 className="greeting"> Congratulations </h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="name">Kiran V</h1>
+      <p className="description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
